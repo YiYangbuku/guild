82aa68e3d95ca8df78fb15a11e068472f25e04ca
yiyang <Yiyang@thoughtworks.com>
2017-10-17 10:45:34 +0800

    update swagger configuration

--- OrderService/src/main/java/com/guild/api/SwaggerConfig.java
+++ OrderService/src/main/java/com/guild/api/SwaggerConfig.java
@@ -2,12 +2,11 @@ package com.guild.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * @author yangyi
@@ -21,7 +20,8 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(regex(".*api.*"))
+//                .paths(regex(".*api.*"))
+                .paths(PathSelectors.any())
                 .build();
     }
 }
